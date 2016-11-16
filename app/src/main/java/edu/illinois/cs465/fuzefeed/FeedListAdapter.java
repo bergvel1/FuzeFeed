fb3f63bfc899f76689537f2c34991b15b6be92b2
David Bergvelt <bergvel1@illinois.edu>
2016-11-16 15:56:49 -0600

    started incorporating post layouts

--- app/src/main/java/edu/illinois/cs465/fuzefeed/FeedListAdapter.java
+++ app/src/main/java/edu/illinois/cs465/fuzefeed/FeedListAdapter.java
@@ -45,14 +45,16 @@ public class FeedListAdapter extends BaseAdapter {
         if (inflater == null)
             inflater = (LayoutInflater) activity
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        /*if (convertView == null)
+            convertView = inflater.inflate(R.layout.feed_item, null);*/
         if (convertView == null)
-            convertView = inflater.inflate(R.layout.feed_item, null);
+            convertView = inflater.inflate(R.layout.facebook_item, null);
 
-        TextView placeholder = (TextView) convertView.findViewById(R.id.placeholder);
+        /*TextView placeholder = (TextView) convertView.findViewById(R.id.placeholder);
 
         Post item = feedItems.get(position);
 
-        placeholder.setText(item.getContent());
+        placeholder.setText(item.getContent());*/
 
         return convertView;
     }
